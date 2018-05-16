beaabf9e61a9002338a336c4990570e37c9502d9
Christian Smith <th3h3ro23@gmail.com>
2018-05-16 19:42:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-var element = ['foo']
+var element = ['foo'];
 
 function addElementToBeginningOfArray(chocolateBars, element){
-  return 
+  return [chocolateBars, ...element];
 }
 
 function destructivelyAddElementToBeginningOfArray(){
